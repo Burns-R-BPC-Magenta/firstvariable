4c1981e2b3855c681c8d588168630574a6ecf003
Burns-R-BPC-Magenta <32098227+Burns-R-BPC-Magenta@users.noreply.github.com>
2017-10-10 13:40:14 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<?php
+//create some strings
+$mytext1 = "Good morning!";
+$mytext2 = "Hello";
+//output string variables on lines
+echo "The first string is";
+echo $mytext1;
+echo "<br></br>";
+echo "The second string is";
+echo $mytext2;
+echo"<br></br>";
+?>
