b602165193733742ad33d6ab4645dd22cc8d42b2
Burns-R-BPC-Magenta <32098227+Burns-R-BPC-Magenta@users.noreply.github.com>
2017-10-10 13:59:57 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -10,4 +10,22 @@ echo "<br></br>";
 echo "The second string is";
 echo $mytext2;
 echo"<br></br>";
+
+
+$posworld = strpos($mytext1, "morning");
+$lngmytext1 = strlen($mytext1);
+$lngmytext2 = strlen($mytext2);
+
+echo "the number of chars in the second string is ";
+echo $lngmytext1;
+echo "<br></br>";
+echo "second string length is ";
+echo $lngmytext2;
+echo "<br></br>";
+echo "the word morning in the first string starts at position ";
+echo $posworld + 1;
+echo " in the first string <br></br>";
+
+
+
 ?>
